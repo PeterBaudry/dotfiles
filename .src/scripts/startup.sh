74c8f02ca5e72ec449fbf19783e31f6f1067143d
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2021-04-21 12:59:33 +0200

    Fix libinput mouse settings

--- .src/scripts/startup.sh
+++ .src/scripts/startup.sh
@@ -7,11 +7,11 @@ autorandr --change &
 
 # Set touchpad options
 # Natural scrolling
-xinput set-prop "DLL0945:00 06CB:CDE6 Touchpad" 331 1
+xinput set-prop "DLL0945:00 06CB:CDE6 Touchpad" 314 1
 # Tap to click
-xinput set-prop "DLL0945:00 06CB:CDE6 Touchpad" 313 1
+xinput set-prop "DLL0945:00 06CB:CDE6 Touchpad" 332 1
 # Disable touchpad while typing
-xinput set-prop "DLL0945:00 06CB:CDE6 Touchpad" 339 1
+xinput set-prop "DLL0945:00 06CB:CDE6 Touchpad" 314 1
 
 # Enable numlock
 #/usr/bin/numlockx on
